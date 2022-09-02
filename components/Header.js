f2a9fd7e81bd1e77c26f605beaec3ec354464973
yashasip <67045923+yashasip@users.noreply.github.com>
2022-09-02 22:16:21 +0530

    Added Header.js Component in new components folder

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+import styles from '../styles/Header.module.css';
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <p className={styles.title}>Academic Feedback</p>
+      </header>
+    </>
+  );
+};
+
+export default Header;
