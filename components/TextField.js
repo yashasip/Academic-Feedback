bff929d42fc95c1e0fd03aac430cbc002cb30fb7
yashasip <67045923+yashasip@users.noreply.github.com>
2022-09-07 21:08:12 +0530

    Added TextField Component

---
+++ components/TextField.js
@@ -0,0 +1,22 @@
+import styles from "../styles/TextField.module.css";
+
+const TextField = (props) => {
+  return (
+    <div className={styles.textFieldSpace}>
+      <label htmlFor={props.id} className={styles.textFieldLabel}>
+        {props.labelText}
+      </label>
+      <input
+        type="text"
+        id={props.id}
+        name={props.id}
+        maxLength="50"
+        readOnly={props.readOnly}
+        required={props.required}
+        className={styles.textField}
+      />
+    </div>
+  );
+};
+
+export default TextField;
