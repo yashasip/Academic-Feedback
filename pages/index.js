c8891efd220f95c3551b8f68293a3e0e74e4d04e
yashasip <67045923+yashasip@users.noreply.github.com>
2022-09-18 14:45:15 +0530

    Added GradeBoard as a separate block

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,24 @@
 import Head from 'next/head';
+import GradeBoard from '../components/GradeBoard';
 import Header from '../components/Header';
 import StudentDetails from '../components/StudentDetails';
 
 import styles from '../styles/Home.module.css';
+import layout from '../styles/Grid.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Academic Feedback</title>
       </Head>
 
       <main className={styles.main}>
         <Header />
-        <StudentDetails />
+        <div className={layout.formGrid}>
+          <StudentDetails />
+          <GradeBoard />
+        </div>
       </main>
     </div>
   );
