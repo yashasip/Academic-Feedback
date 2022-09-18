8ea023a9140ef44954df9e388ba20f8eb4d76988
yashasip <67045923+yashasip@users.noreply.github.com>
2022-09-18 14:03:04 +0530

    Added Rating Slider component

---
+++ components/RatingSlider.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+import { ratings, defaultRating } from "../constants/constants.js"
+
+import styles from "../styles/RatingSlider.module.css";
+import labelStyle from "../styles/Board.module.css";
+
+const RatingSlider = (props) => {
+    const [ratingIndex, setRatingIndex] = useState(defaultRating);
+  return (
+    <div className={styles.ratingSliderSpace}>
+          <label for={props.id} className={labelStyle.labelText} >{ ratings[ratingIndex] }</label><br/>
+          <input type="range" id={props.id} name={props.id} className={styles.ratingSlider} min={1} max={5} onChange={ e=> setRatingIndex(e.target.value - 1) } />
+    </div>
+  );
+};
+
+export default RatingSlider;
