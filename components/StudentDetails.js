b24df96ec0f66bb8d99372f0be3e6648ceb58d6c
yashasip <67045923+yashasip@users.noreply.github.com>
2022-09-18 14:41:13 +0530

    Removed Redundant Text Fieldds

--- components/StudentDetails.js
+++ components/StudentDetails.js
@@ -1,7 +1,7 @@
 import Button from "../components/Button.js";
 import TextField from "../components/TextField";
 
-import styles from "../styles/StudentDetails.module.css";
+import styles from "../styles/Board.module.css";
 import layout from "../styles/Grid.module.css";
 
 export default function StudentDetails() {
@@ -12,8 +12,6 @@ export default function StudentDetails() {
         <hr className={styles.tendi} />
         <form className={layout.studentFormGrid}>
           <TextField id="studentId" labelText="ID"/>
-          <TextField id="academicYear" labelText="Academic Year"/>
-          <TextField id="section" labelText="Section"/>
           <TextField id="semester" labelText="Semester"/>
           <Button type="submit" text="Submit" layout={layout.gridColSpanTwo} />
         </form>
