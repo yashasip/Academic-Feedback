702ccb8ba6b9fe47943bbc3e54dfcd084a05e261
yashasip <67045923+yashasip@users.noreply.github.com>
2022-09-18 14:12:27 +0530

    Added LikertScale component

---
+++ components/LikertScale.js
@@ -0,0 +1,14 @@
+import RatingSlider from "./RatingSlider";
+
+import styles from "../styles/LikertScale.module.css"
+
+const LikertScale = (props) => {
+    return (
+        <div className={styles.likertScaleSpace}>
+            <label for={props.id} className={styles.question}>{props.question}</label>
+            <RatingSlider id={props.id} />
+        </div>  
+    );
+}
+
+export default LikertScale;
