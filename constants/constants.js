7f3425299a4ece4592fddb6f5dcecaa0f7c85db7
ShreyashPu <19cs154.shreyash@sjec.ac.in>
2022-09-18 21:55:16 +0530

    Added questions and ratings in constants/constants.js

--- constants/constants.js
+++ constants/constants.js
@@ -1,11 +1,28 @@
-const ratings = ["worst", "bad", "average", "good", "best"];
+const ratings = ["Below Average", "Average", "Good", "Very Good", "Excellent"];
 const defaultRating = 2;
 
 const questions = [
-  "Complete syllabus of the course in time",
+  "Completes syllabus of the course in time",
+  "Scheduled organization of assignments, class test, quizzes and seminars",
   "Focus on Syllabi",
   "Self Confidence",
-  "Shares the answers of class tests or sessional test questions ager the conduct of the class tests/sessional tests.",
+  "Communication skills",
+  "Conducting the classroom discussions",
+  "Teaching the subject matter/Delivery of structured lecture",
+  "Skill of linking subject to life experience & creating interest in the subject",
+  "Refers to latest developments in the field",
+  "Shares the answers of class tests or sessional test questions after the conduct of the class tests/sessional tests",
+  "Makes sure that he/she is being understood",
+  "Helping approach towards varied academic interests of students",
+  "Helps student in providing study material which is not readily available in the text books say through e-resources, e-journals, reference books, open course wares etc.",
+  "Helps students irrespective of ethnicity and culture/background",
+  "Helps students facing physical, emotional and learning challenges",
+  "Approach towards developing professional skills among students",
+  "Helps students in realizing their strengths and developmental needs",
+  "Control mechanism in effectively conducting the class",
+  "Skills of addressing inappropriate behavior of student",
+  "Tendency of inviting opinion and question on subject matter from students"
+
 ];
 
 export { ratings, defaultRating, questions };
