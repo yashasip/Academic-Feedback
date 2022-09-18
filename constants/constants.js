90606366518041d8c47d639f0c3a8540d17df09a
yashasip <67045923+yashasip@users.noreply.github.com>
2022-09-18 13:56:52 +0530

    Added constants/ directory

---
+++ constants/constants.js
@@ -0,0 +1,12 @@
+const ratings = ["worst", "bad", "average", "good", "best"];
+const defaultRating = 2;
+
+const questions = [
+  "Complete syllabus of the course in time",
+  "Focus on Syllabi",
+  "Self Confidence",
+  "Shares the answers of class tests or sessional test questions ager the conduct of the class tests/sessional tests.",
+];
+
+export { ratings, defaultRating, questions };
+
