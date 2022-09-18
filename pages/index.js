c93d3f31be6d0444f190de8944b625930981d8ec
yashasip <67045923+yashasip@users.noreply.github.com>
2022-09-18 22:30:57 +0530

    Added State and handled it

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from 'next/head';
+import { useState } from 'react';
+
 import GradeBoard from '../components/GradeBoard';
 import Header from '../components/Header';
 import StudentDetails from '../components/StudentDetails';
@@ -7,6 +9,15 @@ import styles from '../styles/Home.module.css';
 import layout from '../styles/Grid.module.css';
 
 export default function Home() {
+  const [studentVerified, setVerified] = useState(false);
+
+  const [batchData, setBatchData] = useState({});
+  function getData(fetchedData) {
+    setBatchData(fetchedData)
+    console.log(batchData);
+    setVerified(true); // display GradeBoard
+  }
+
   return (
     <div>
       <Head>
@@ -16,8 +27,10 @@ export default function Home() {
       <main className={styles.main}>
         <Header />
         <div className={layout.formGrid}>
-          <StudentDetails />
-          <GradeBoard />
+          <StudentDetails studentVerified={studentVerified} fetchDataHandle={ getData } /> 
+          {studentVerified ? // display GradeBoard only after submission accepted
+            <GradeBoard batchData={ batchData } /> : null
+          }
         </div>
       </main>
     </div>
