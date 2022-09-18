1c48ba2eb64c41d8fc115e3abb5c501ced1ace26
yashasip <67045923+yashasip@users.noreply.github.com>
2022-09-18 22:23:35 +0530

    Added props batchData and handled it

--- components/GradeBoard.js
+++ components/GradeBoard.js
@@ -6,17 +6,29 @@ import { questions } from "../constants/constants.js";
 
 import styles from "../styles/Board.module.css";
 import layout from "../styles/Grid.module.css";
+import { useState } from "react";
 
-const options = ["OS", "DC", "ATC", "MIV", "ADE"]; // dummy values
+export default function GradeBoard(props) {
+  let subjects = [];
+  for (let key in props.batchData) {
+    // creates list of subjects
+    subjects.push(key);
+  }
 
-export default function GradeBoard() {
+  const [teacher, setTeacher] = useState(props.batchData[subjects[0]]); // assumes batchdata is not empty.
+
   return (
     <>
       <section className={styles.gradeBoard}>
         <p className={styles.heading}>Grade Academic Performance</p>
         <hr className={styles.tendi} />
-        <SelectMenu id="subject" labelText="Subject:" choices={options} />
-        <p className={styles.labelText}>Teacher: </p>
+        <SelectMenu
+          id="subject"
+          labelText="Subject:"
+          choices={subjects}
+          onChange={(e) => setTeacher(props.batchData[e.target.value])}
+        />
+        <p className={styles.labelText}>Teacher: {teacher} </p>
         <hr className={styles.fullTendi} />
         <ol className={styles.questionSpace}>
           {questions.map((question) => (
