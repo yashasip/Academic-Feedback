cffeee6b73e25dbdcf2c488f4f85f603cfc6ba12
yashasip <67045923+yashasip@users.noreply.github.com>
2022-09-18 14:11:21 +0530

    Added SelectMenu component

---
+++ components/SelectMenu.js
@@ -0,0 +1,17 @@
+import styles from "../styles/SelectMenu.module.css"
+import labelStyle from "../styles/Board.module.css"
+
+const SelectMenu = (props) => {
+  return (
+      <div className={styles.label}>
+      <label for={props.id} className={labelStyle.labelText}>{props.labelText}</label>
+      <select id={props.id} name={props.id} className={styles.selectMenu}>
+        {props.choices.map((choice) => (
+          <option value={choice}>{choice}</option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default SelectMenu;
