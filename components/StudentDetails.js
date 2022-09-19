4da59d471ff81205ed5077f4469e506abf315291
yashasip <67045923+yashasip@users.noreply.github.com>
2022-09-19 17:33:10 +0530

    Added Student Details onChange

--- components/StudentDetails.js
+++ components/StudentDetails.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import Button from "../components/Button.js";
 import TextField from "../components/TextField";
+import SelectMenu from "./SelectMenu.js";
+
+import { semesters } from "../constants/constants.js";
 
 import styles from "../styles/Board.module.css";
 import layout from "../styles/Grid.module.css";
@@ -8,14 +11,15 @@ import layout from "../styles/Grid.module.css";
 export default function StudentDetails(props) {
   const [studentDetails, setStudentDetails] = useState({
     student_id: "",
-    semester: ""
+    semester: "1",
   });
 
   const onChange = (e) => {
-    setStudentDetails({ ...studentDetails, [e.target.id]: e.target.value });
-  }
+    setStudentDetails({ ...studentDetails, [e.target.name]: e.target.value });
+  };
 
   const submitDetails = () => {
+    console.log(studentDetails);
     props.fetchDataHandle({
       OS: "Teacher1",
       DC: "Teacher2",
@@ -37,10 +41,12 @@ export default function StudentDetails(props) {
             readOnly={props.studentVerified}
             onChange={onChange}
           />
-          <TextField
+          <SelectMenu
             id="semester"
             labelText="Semester"
             readOnly={props.studentVerified}
+            choices={semesters}
+            onChange={onChange}
           />
 
           {props.studentVerified ? null : ( // button disappears after submission
