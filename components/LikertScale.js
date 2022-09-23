aad2abf018311ca7d0ff2ff842971b5509c1b337
yashasip <67045923+yashasip@users.noreply.github.com>
2022-09-23 20:36:31 +0530

    Added attributes for Rating Slider props in LikertScale.js

--- components/LikertScale.js
+++ components/LikertScale.js
@@ -1,14 +1,31 @@
+import { useState } from "react";
+
 import RatingSlider from "./RatingSlider";
 
-import styles from "../styles/LikertScale.module.css"
+import { defaultRating } from "../constants/constants";
+
+import styles from "../styles/LikertScale.module.css";
 
 const LikertScale = (props) => {
-    return (
-        <div className={styles.likertScaleSpace}>
-            <label htmlFor={props.id} className={styles.question}>{props.question}</label>
-            <RatingSlider id={props.id} />
-        </div>  
-    );
-}
+  const [ratingValue, setRatingValue] = useState(defaultRating + 1);
+
+  const onChange = (e) => {
+    let index = parseInt(e.target.id.slice(1));
+    props.ratingCallback(e.target.id, e.target.value);
+  };
+
+  return (
+    <div className={styles.likertScaleSpace}>
+      <label htmlFor={props.id} className={styles.question}>
+        {props.question}
+      </label>
+      <RatingSlider
+        id={props.id}
+        onChange={onChange}
+        value={props.ratingValue}
+      />
+    </div>
+  );
+};
 
 export default LikertScale;
