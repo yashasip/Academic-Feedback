1466ad7d10baf2738176e4155df788e68cbd3ef9
yashasip <67045923+yashasip@users.noreply.github.com>
2022-09-23 20:26:19 +0530

    Updated RatingSlider.js to include more attributes

--- components/RatingSlider.js
+++ components/RatingSlider.js
@@ -1,16 +1,32 @@
 import { useState } from "react";
 
-import { ratings, defaultRating } from "../constants/constants.js"
+import { ratings, defaultRating } from "../constants/constants.js";
 
 import styles from "../styles/RatingSlider.module.css";
 import labelStyle from "../styles/Board.module.css";
 
 const RatingSlider = (props) => {
-    const [ratingIndex, setRatingIndex] = useState(defaultRating);
+  const [ratingIndex, setRatingIndex] = useState(defaultRating);
+  ratingIndex = props.value - 1; // too coupled
   return (
     <div className={styles.ratingSliderSpace}>
-          <label htmlFor={props.id} className={labelStyle.labelText} >{ ratings[ratingIndex] }</label><br/>
-          <input type="range" id={props.id} name={props.id} className={styles.ratingSlider} defaultValue={3} min={1} max={5} onChange={ e=> setRatingIndex(e.target.value - 1) } />
+      <label htmlFor={props.id} className={labelStyle.labelText}>
+        {ratings[ratingIndex]}
+      </label>
+      <br />
+      <input
+        type="range"
+        id={props.id}
+        name={props.id}
+        className={styles.ratingSlider}
+        value={props.value}
+        min={1}
+        max={5}
+        onChange={(e) => {
+          setRatingIndex(e.target.value - 1);
+          props.onChange(e);
+        }}
+      />
     </div>
   );
 };
