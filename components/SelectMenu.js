96ba5238e990bb2975ffd9da747d4db7c432dd6a
yashasip <67045923+yashasip@users.noreply.github.com>
2022-10-04 20:57:08 +0530

    Implemented Reactive SelectMenu using useRef Hook

--- components/SelectMenu.js
+++ components/SelectMenu.js
@@ -1,25 +1,48 @@
 import styles from "../styles/SelectMenu.module.css";
 import labelStyle from "../styles/Board.module.css";
+import { useImperativeHandle, useState,useRef, forwardRef } from "react";
 
-const SelectMenu = (props) => {
+const SelectMenu = ({ id, labelText, disabled, choices, onChange }, ref ) => {
+  const [options, setOptions] = useState(choices);
+  const selectRef = useRef();
+  
+  useImperativeHandle(ref, () => {
+    return {
+      removeOption: () => {
+        console.log(selectRef.current.options);
+        selectRef.current.options.remove(selectRef.current.selectedIndex);
+      },
+      isEmpty: () => {
+        if (selectRef.current.options.length == 0) return true;
+        return false;
+      },
+      getCurrentValue: () => {
+        return selectRef.current.value;
+      }
+    };
+  },[]);
+
   return (
-    <div className={styles.label}>
-      <label htmlFor={props.id} className={labelStyle.labelText}>
-        {props.labelText}
+    <div className={styles.label} ref={ref}>
+      <label htmlFor={id} className={labelStyle.labelText}>
+        {labelText}
       </label>
       <select
-        id={props.id}
-        name={props.id}
+        id={id}
+        name={id}
         className={styles.selectMenu}
-        onChange={(e) => props.onChange(e)}
-        disabled={props.disabled}
+        onChange={(e) => onChange(e)}
+        disabled={disabled}
+        ref={selectRef}
       >
-        {props.choices.map((choice, i) => (
-          <option key={i+1} value={choice}>{choice}</option>
+        {options.map((choice, i) => (
+          <option key={i + 1} value={choice}>
+            {choice}
+          </option>
         ))}
       </select>
     </div>
   );
 };
 
-export default SelectMenu;
+export default forwardRef(SelectMenu);
